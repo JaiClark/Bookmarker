6aa8290f64b2832c36a7ffc9868d92ed3ac3fc0e
Jaurs <rhsemail01@gmail.com>
2018-11-15 06:00:56 -0500

    Broke the app

--- js/main.js
+++ js/main.js
@@ -1,42 +1,57 @@
 // listen for form submit
-window.onload=function(){
- 
-document.getElementById('myForm').addEventListener('submit', saveBookmark);
+  document.getElementById("myForm").addEventListener('submit', saveBookmark);
 
-//Save Bookmark
-function saveBookmark(e){
+  //Save Bookmark
+  function saveBookmark() {
     // get form values
-    var siteName =document.getElementById('siteName').value;
-    var siteURL =document.getElementById('siteURL').value;
+    var siteName = document.getElementById('siteName').value;
+    var siteURL = document.getElementById('siteURL').value;
+
 
     var bookmark = {
-        name: siteName,
-        url: siteURL
+      name: siteName,
+      url: siteURL
     }
-    
-
-    /*
-    // Local Storage Test
-    //Local Storage only stores strings
-    localStorage.setItem('test', "Hello World");
-    console.log(localStorage.getItem('test'));
-    localStorage.removeItem('test');
-    console.log(localStorage.getItem('test'));
-    */
 
     //Test if bookmarks is null
-    if(localStorage.getItem('bookmarks') === null){
-        //Init array
-        var bookmarks = [];
-        // Add to array
+    if (localStorage.getItem('bookmarks') === null) {
+      //Init array
+      var bookmarks = [];
+      // Add to array
+      bookmarks.push(bookmark);
+      // Set to Localstorage
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        // get bookmarks from local storage
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        // add bookmark to array
         bookmarks.push(bookmark);
-        // Set to Localstorage
+        // Re-set back to localStorage
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 
-
-    //prevent form from submitting
     e.preventDefault();
-}
+  }
 
+
+// //fetch Bookmarks
+function fetchBookmarks(){
+//get bookmarks from LocalStorage
+var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+//Get output id
+var bookmarksResults = document.getElementById('bookmarksResults');
+
+//Build output
+bookmarksResults.innerHTML = "" ;
+for(var i = 0; i < bookmarks.length; i++){
+  var name = bookmarks[i].name;
+  var url = bookmarks[i].url;
+
+  bookmarksResults.innerHTML += '<div class="well">'+
+                                '<h3>'+name+
+                                ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+                                ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                '</h3>'+
+                                '</div>';
+}
 }
